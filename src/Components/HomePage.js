3eef89435e4de928c2ef2d9c2ae46e319faba87c
Kztech <pathmaker.525@gmail.com>
2019-10-03 11:12:09 -0400

    Project Setup

---
+++ src/Components/HomePage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './HomePage.scss'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Bikes</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Bikes</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Bikes</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Bikes</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Bikes</h1>
+                        <span className="subtitle">Shop now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
