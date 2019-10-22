aa419ed64b516d26dc7a193e3c752793074240f2
Kztech <pathmaker.525@gmail.com>
2019-10-22 13:35:43 -0400

    removing redux-logger from production build

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# powersports_shop
+# EShop
+
+React-Redux based web shop with Firebase database and authentication.
+
+To run the project: npm i, npm start
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-//Add LocatStore library - browser cashing store.(store, root-reducer, index)
+//Add LocalStore library - browser cashing store.(store, root-reducer, index)
 import { persistStore } from 'redux-persist';
 
-const middlewares = [logger];
+const middlewares = [];
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
